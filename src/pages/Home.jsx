3caf9e3a4095c8c3fe6f10dd8debda679c16a0b5
hoagbaodinh <46434823+hoagbaodinh@users.noreply.github.com>
2024-09-05 15:03:54 +0700

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import Banner from '../components/HomePage/Banner';
 import Categories from '../components/HomePage/Categories';
 import Products from '../components/HomePage/Products';
 import Service from '../components/HomePage/Service';
-import Newsletter from '../components/HomePage/Newsletter';
 
 const HomePage = () => {
   return (
